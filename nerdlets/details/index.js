fb1d7d0853420cf49b9d4c522544b9fec88865b0
Joel Worrall <jworrall@newrelic.com>
2019-09-09 19:14:54 -0400

    Added a ChartGroup to the details pane.

--- nerdlets/details/index.js
+++ nerdlets/details/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Grid, GridItem, BillboardChart, LineChart, HeadingText, BlockText } from "nr1";
+import { Grid, GridItem, ChartGroup, BillboardChart, LineChart, HeadingText, BlockText } from "nr1";
 import { getJourneys } from '../../journeyConfig';
 const journeyConfig = getJourneys();
 import moment from 'moment';
@@ -39,42 +39,44 @@ export default class Details extends React.Component {
     }
 
     return (
-      <Grid style={{ margin: '20px'}}>
-        <GridItem columnSpan="12">
-          <HeadingText type='heading3' className="detailPaneHeader customerJourneyBreadcrumbs">
-            <span className="customerJourneyBreadcrumb">{journey.title}</span>
-            <span className="customerJourneyBreadcrumb">{column.label}</span>
-            <span className="customerJourneyBreadcrumb">{step.label}</span>
-          </HeadingText>
-        </GridItem>
-        {kpis && <GridItem columnSpan="12">
-          <HeadingText>KPI's</HeadingText>
-        </GridItem>}
-        {kpis && kpis.map((kpi, i) => {
-          return <GridItem key={i} columnSpan="4" className="chartContainer" >
-            <HeadingText type="heading3">{kpi.label}</HeadingText>
-            <BillboardChart accountId={journey.accountId} query={kpi.nrql} className="chart" />
-            <BlockText>{kpi.description}</BlockText>
+      <ChartGroup>
+        <Grid style={{ margin: '20px'}}>
+          <GridItem columnSpan="12">
+            <HeadingText type='heading3' className="detailPaneHeader customerJourneyBreadcrumbs">
+              <span className="customerJourneyBreadcrumb">{journey.title}</span>
+              <span className="customerJourneyBreadcrumb">{column.label}</span>
+              <span className="customerJourneyBreadcrumb">{step.label}</span>
+            </HeadingText>
           </GridItem>
-        })}
-        <GridItem columnSpan="12">
-          <HeadingText className="timeSeriesHeading">Timeseries Data</HeadingText>
-          <BlockText className="timePeriodText">Since {moment.duration(duration).format()} ago</BlockText>
-        </GridItem>
-        {stats.map((stat, i) => {
-          const query =
-            stat.value.nrql +
-            ` AND (${step.nrqlWhere}) AND (${
-            column.nrqlWhere
-            }) TIMESERIES SINCE ${durationInMinutes} MINUTES AGO COMPARE WITH ${durationInMinutes *
-            2} MINUTES AGO`
-          // console.log(query);
-          return <GridItem key={i} columnSpan="4" className="chartContainer" >
-            <HeadingText type="heading3">{stat.label}</HeadingText>
-            <LineChart accountId={journey.accountId} query={query} className="chart" />
+          {kpis && <GridItem columnSpan="12">
+            <HeadingText>KPI's</HeadingText>
+          </GridItem>}
+          {kpis && kpis.map((kpi, i) => {
+            return <GridItem key={i} columnSpan="4" className="chartContainer" >
+              <HeadingText type="heading3">{kpi.label}</HeadingText>
+              <BillboardChart accountId={journey.accountId} query={kpi.nrql} className="chart" />
+              <BlockText>{kpi.description}</BlockText>
+            </GridItem>
+          })}
+          <GridItem columnSpan="12">
+            <HeadingText className="timeSeriesHeading">Timeseries Data</HeadingText>
+            <BlockText className="timePeriodText">Since {moment.duration(duration).format()} ago</BlockText>
           </GridItem>
-        })}
-      </Grid>
+          {stats.map((stat, i) => {
+            const query =
+              stat.value.nrql +
+              ` AND (${step.nrqlWhere}) AND (${
+              column.nrqlWhere
+              }) TIMESERIES SINCE ${durationInMinutes} MINUTES AGO COMPARE WITH ${durationInMinutes *
+              2} MINUTES AGO`
+            // console.log(query);
+            return <GridItem key={i} columnSpan="4" className="chartContainer" >
+              <HeadingText type="heading3">{stat.label}</HeadingText>
+              <LineChart accountId={journey.accountId} query={query} className="chart" />
+            </GridItem>
+          })}
+        </Grid>
+      </ChartGroup>
     );
   }
 }
