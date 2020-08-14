84d703c473368295bd131091b53010a590f3e09c
artur.wozniak <wozniaka83@gmail.com>
2020-08-14 16:23:42 +0200

    chore: step one with formik

--- nerdlets/components/new-journey/steps-forms/step-one/step-one.js
+++ nerdlets/components/new-journey/steps-forms/step-one/step-one.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { TextField } from 'nr1';
+import { TextField, Button } from 'nr1';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 import StepForm from '../step-form';
 
 class StepOne extends Component {
@@ -7,28 +9,51 @@ class StepOne extends Component {
     return (
       <StepForm title="Basic Info">
         <div style={{ width: '50%' }}>
-          <TextField
-            label="Title"
-            style={{ marginBottom: '16px' }}
-            value=""
-            onChange={() => console.log('title changed')}
-          />
-          <TextField
-            label="Funnel event"
-            style={{ marginBottom: '16px' }}
-            value=""
-            onChange={() => console.log('funnel event')}
-          />
-          <TextField
-            label="Funnel measure"
-            style={{ marginBottom: '16px' }}
-            value=""
-            onChange={() => console.log('Funnel measure')}
-          />
+          <Formik
+            initialValues={{ title: '', funnel_event: '', funnel_measure: '' }}
+            validationSchema={Yup.object().shape({
+              title: Yup.string().required('Is required'),
+              funnel_event: Yup.string().required('Is required'),
+              funnel_measure: Yup.string().required('Is required')
+            })}
+            onSubmit={values => {
+              console.log('StepOne -> render -> values', values);
+            }}
+          >
+            {({ values, errors, setFieldValue, handleSubmit }) => (
+              <form onSubmit={handleSubmit}>
+                <TextField
+                  label="Title"
+                  style={{ marginBottom: '16px' }}
+                  value={values.title}
+                  onChange={e => setFieldValue('title', e.target.value)}
+                  invalid={errors.title}
+                />
+                <TextField
+                  label="Funnel event"
+                  style={{ marginBottom: '16px' }}
+                  value={values.funnel_event}
+                  onChange={e => setFieldValue('funnel_event', e.target.value)}
+                  invalid={errors.funnel_event}
+                />
+                <TextField
+                  label="Funnel measure"
+                  style={{ marginBottom: '16px' }}
+                  value={values.funnel_measure}
+                  onChange={e =>
+                    setFieldValue('funnel_measure', e.target.value)
+                  }
+                  invalid={errors.funnel_measure}
+                />
+                <Button type={Button.TYPE.PRIMARY} onClick={handleSubmit}>
+                  Submit
+                </Button>
+              </form>
+            )}
+          </Formik>
         </div>
       </StepForm>
     );
   }
 }
-
 export default StepOne;
